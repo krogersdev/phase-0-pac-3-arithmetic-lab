3d5d54c01127a5f05668f02c911acea968f474ec
Kevin Rogers <kevinarogers1@gmail.com>
2021-08-27 17:09:56 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a,b) {
+    return (a+b);
+}
+
+function subtract(a,b) {
+    return (a-b);
+}
+
+function multiply(a,b) {
+    return (a*b);
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function increment(n) {
+    n++
+    return n;
+}
+
+function decrement(n) {
+    n--
+    return n;
+}
+
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+
+}
+
+
+
+
+
+
+
+
